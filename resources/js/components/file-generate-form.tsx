000525be679e5ee3ae6fe4ef0ed0491af2d3ea9d
ChrisRoach1 <c.roach10@yahoo.com>
2025-10-27 20:21:17 -0400

    close modal when generating

--- resources/js/components/file-generate-form.tsx
+++ resources/js/components/file-generate-form.tsx
@@ -38,6 +38,7 @@ export default function FileGenerateForm({ isOpen, onOpenChange, fileName, fileU
         post('/generateFromFile', {
             onSuccess: () => {
                 router.flushAll();
+                onOpenChange(false);
             }
         })
     };
