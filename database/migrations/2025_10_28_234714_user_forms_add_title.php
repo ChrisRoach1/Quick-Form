9fd2f42e4a8c30e9c0d64e963dfde723a6ed51f1
ChrisRoach1 <c.roach10@yahoo.com>
2025-10-28 20:06:15 -0400

    make title field nullable

--- database/migrations/2025_10_28_234714_user_forms_add_title.php
+++ database/migrations/2025_10_28_234714_user_forms_add_title.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('user_forms', function (Blueprint $table): void {
-            $table->string('title')->nullable(false);
+            $table->string('title')->nullable(true);
         });
     }
 
